4d3e7c079277a4dcc0668b53e872797c64382277
jstone18 <jason.stone0808@gmail.com>
2019-05-27 16:22:50 -0400

    Add validations to user model, create 'new' template for future sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase}
   has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8}
 end
