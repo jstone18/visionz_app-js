9284d3f66fcd4f03c9984be09b97dfc53b8159b3
jstone18 <jason.stone0808@gmail.com>
2019-05-28 00:36:56 -0400

    Complete #create action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
