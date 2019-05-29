d8ee2068ec25d0f5f67727776ea769eac773808e
jstone18 <jason.stone0808@gmail.com>
2019-05-29 17:40:48 -0400

    Update functionality to CRUD actions in PostController. Create index, show, new and edit views for posts and new/edit forms for posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
-
   end
 
   def new
@@ -21,26 +22,31 @@ class PostsController < ApplicationController
     end
   end
 
-  def method_name
-
-  end
-
   def edit
 
   end
 
   def update
-
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :link, :description)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
