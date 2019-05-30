61765a1281de348a610d286d73160c4a49769fd7
jstone18 <jason.stone0808@gmail.com>
2019-05-30 19:34:36 -0400

    Refactor headining on post edit page, add user name to show page, delete un-needed password validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,4 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6}
 end
