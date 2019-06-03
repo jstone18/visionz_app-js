10b312a8584e5601b7dcfc8c2060fcd8dbdf3846
jstone18 <jason.stone0808@gmail.com>
2019-06-03 16:46:47 -0400

    Create show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  resources :users
+  resources :users, only: [:show]
 
   resources :posts do
     member do
