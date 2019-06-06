ad5ba052cb9bff90acaf56e535cc75e5516fb048
jstone18 <jason.stone0808@gmail.com>
2019-06-05 21:26:55 -0400

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,13 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
+  validates :title, presence: true, length: { minimum: 3 }, length: { maximum: 25 }
+  validates :description, presence: true
+
   has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
   validates :image, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :validatable
-
   before_save { self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/ 
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
