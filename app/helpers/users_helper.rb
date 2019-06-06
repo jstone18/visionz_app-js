4d99f8d36a99dc525f16026497a5d15591acdd45
jstone18 <jason.stone0808@gmail.com>
2019-06-05 21:22:39 -0400

    Create User helper methods and use to refactor views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+
+  def profile_pic
+    if @user.avatar.exists?
+      image_tag @user.avatar.url, class: "avatar"
+    else
+      image_tag("avatar_png_71508", class: "avatar")
+    end
+  end
+
+  def edit_profile
+    if @user == current_user
+      link_to "Edit Profile", edit_user_path(@user), class: "edit-profile button"
+    end
+  end
+
 end
