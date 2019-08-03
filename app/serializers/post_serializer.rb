56eb3fa148498d2fa624d96c61064b1d81ed503e
jstone18 <jason.stone0808@gmail.com>
2019-08-02 20:45:41 -0400

    Add csrf protection gem

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :user_id, :updated_at, :image
-
+  attributes :id, :title, :description, :user_id, :updated_at
+  
   belongs_to :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :bio
-
+  
   has_many :posts
 end
