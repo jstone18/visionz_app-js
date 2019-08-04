f7696a03852f2defb1ef2cdf2eba107eca8226f8
jstone18 <jason.stone0808@gmail.com>
2019-08-03 21:14:50 -0400

    Move js code for user show page into asset pipline. Change style of 'Read More' button on post show page to match other buttons

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,20 @@
+// JS script for Show Images button
+$(function() {
+  $("#js-post").on("click", function() {
+    const userId = $(this).data("uid");
+    $.get("/users/" + userId + ".json", function(data) {
+      let post = data.posts.map(p => {
+        return `
+          <div class="wrapper">
+            <h6 style="color:gray;text-align: center;"><u>${p.title}</u></h6>
+            <a href="<%= user_post_path(@user.posts, @post) %>">
+              <img src="${p.image}"><br>
+            </a>
+          </div>
+        `
+      })
+      $(".user-images").html(post)
+      $(".user-title").html(post)
+    })
+  })
+})
