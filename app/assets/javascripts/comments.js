0d64da14655f77028b21dfb7a55de75e80f80e62
jstone18 <jason.stone0808@gmail.com>
2019-08-10 17:04:03 -0400

    Refactor Ajax code using jquery 'get' method

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
   $("a.load_comments").on("click", function(e) {
-    $.ajax({
-      method: "GET",
-      url: this.href
-    }).done(function(response) {
-        $("div.post-comments").html(response);
-      });
+    $.get(this.href).success(function(response) {
+      $("div.post-comments").html(response)
+    })
 
     e.preventDefault();
   })
